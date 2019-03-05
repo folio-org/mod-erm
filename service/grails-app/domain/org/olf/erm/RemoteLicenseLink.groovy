b85c06e15ae63b98c6efce9bb2f2243b213555cb
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-03-05 11:55:34 +0000

    Add the remote id.

--- service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
+++ service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
@@ -6,7 +6,9 @@ import com.k_int.web.toolkit.refdata.RefdataValue
 import grails.gorm.MultiTenant
 
 public class RemoteLicenseLink implements MultiTenant<RemoteLicenseLink> {
-  String id
+  String id // This is the internal ID of the link and not the ID of the remote license.
+  
+  String remoteId
   
   @Defaults(['Controlling', 'Future', 'Historical'])
   RefdataValue status
@@ -14,14 +16,16 @@ public class RemoteLicenseLink implements MultiTenant<RemoteLicenseLink> {
   String note
   
   static mapping = {
-             id column:'rll_id', generator: 'uuid', length:36
+             id column:'rll_id', generator: 'uuid', length: 36
+       remoteId column:'rll_remote_id', length: 50 // UUIDs are 36 chars. Allowing some wriggle-room here..
         version column:'rll_version'
          status column:'rll_status'
            note column:'rll_note', type: 'text'
   }
   
   static constraints = {
-          licenseType (nullable:false)
+             remoteId (nullable:false, blank:false)
+               status (nullable:false)
                  note (nullable:true, blank:false)
   }
 }
