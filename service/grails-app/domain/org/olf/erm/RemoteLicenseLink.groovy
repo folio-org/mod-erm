bfbc0ca6b77b531f8b3b02478928e6d57bf49248
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-03-06 10:41:59 +0000

    Implement method. THis will probably change in the future, but OK now.

--- service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
+++ service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
@@ -1,14 +1,12 @@
 package org.olf.erm;
 
+import com.k_int.okapi.remote_resources.RemoteOkapiLink
 import com.k_int.web.toolkit.refdata.Defaults
 import com.k_int.web.toolkit.refdata.RefdataValue
 
 import grails.gorm.MultiTenant
 
-public class RemoteLicenseLink implements MultiTenant<RemoteLicenseLink> {
-  String id // This is the internal ID of the link and not the ID of the remote license.
-  
-  String remoteId
+public class RemoteLicenseLink extends RemoteOkapiLink implements MultiTenant<RemoteLicenseLink> {
   
   @Defaults(['Controlling', 'Future', 'Historical'])
   RefdataValue status
@@ -16,16 +14,16 @@ public class RemoteLicenseLink implements MultiTenant<RemoteLicenseLink> {
   String note
   
   static mapping = {
-             id column:'rll_id', generator: 'uuid', length: 36
-       remoteId column:'rll_remote_id', length: 50 // UUIDs are 36 chars. Allowing some wriggle-room here..
-        version column:'rll_version'
          status column:'rll_status'
            note column:'rll_note', type: 'text'
   }
   
   static constraints = {
-             remoteId (nullable:false, blank:false)
                status (nullable:false)
                  note (nullable:true, blank:false)
   }
-}
+
+  @Override
+  public String remoteUri() {
+    return 'licenses/licenses';
+  }}
