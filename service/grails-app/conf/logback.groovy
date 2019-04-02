a7ff6dbfe92837d71870f4e507e0f47c2ed42ab1
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-04-02 17:45:19 +0100

    Reduce logging.

--- service/grails-app/conf/logback.groovy
+++ service/grails-app/conf/logback.groovy
@@ -38,15 +38,15 @@ logger ('com.k_int.web.toolkit.utils.RequestUtils', DEBUG)
 
 logger ('com.k_int.okapi.remote_resources.RemoteOkapiLinkListener', DEBUG)
 
-logger("org.hibernate.SQL", DEBUG)
-logger("org.hibernate.type.descriptor.sql.BasicBinder", TRACE)
+//logger("org.hibernate.SQL", DEBUG)
+//logger("org.hibernate.type.descriptor.sql.BasicBinder", TRACE)
 
 
 // Uncomment below logging for output of OKAPI client http.
-logger ('com.k_int.okapi.OkapiClient', TRACE)
-logger 'groovy.net.http.JavaHttpBuilder', DEBUG
-logger 'groovy.net.http.JavaHttpBuilder.content', DEBUG
-logger 'groovy.net.http.JavaHttpBuilder.headers', DEBUG
+logger ('com.k_int.okapi.OkapiClient', DEBUG)
+//logger 'groovy.net.http.JavaHttpBuilder', DEBUG
+//logger 'groovy.net.http.JavaHttpBuilder.content', DEBUG
+//logger 'groovy.net.http.JavaHttpBuilder.headers', DEBUG
 
 if (Environment.currentEnvironment == Environment.TEST) {
   logger 'groovy.net.http.JavaHttpBuilder', DEBUG
