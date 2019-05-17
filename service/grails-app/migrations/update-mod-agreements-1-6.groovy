2ee13dd7176333ee41a6f35c251a9ed1cc7c3399
John Fereira <jaf30@cornell.edu>
2019-05-17 09:37:56 -0400

    change TEXT to CLOB for sa_description and res_description

--- service/grails-app/migrations/update-mod-agreements-1-6.groovy
+++ service/grails-app/migrations/update-mod-agreements-1-6.groovy
@@ -1,32 +1,20 @@
 databaseChangeLog = {
-  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-4") {
-      modifyDataType(tableName: "custom_property_definition", columnName: "pd_description", newDataType: "TEXT")
-  }
-
-  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-5") {
+  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-1") {
       modifyDataType( 
         tableName: "erm_resource", 
         columnName: "res_description", 
-        newDataType: "TEXT",
+        newDataType: "CLOB",
         confirm: "Successfully updated the res_description column."
       )
   }
 
-  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-6") {
+  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-2") {
       modifyDataType( 
         tableName: "subscription_agreement", 
         columnName: "sa_description", 
-        newDataType: "TEXT", 
+        newDataType: "CLOB", 
         confirm: "Successfully updated the sa_description column."
       )
   }
 
-  changeSet(author: "John Fereira", id: "2019-05-14-ERM-218-7") {
-      modifyDataType( 
-         tableName: "refdata_category", 
-         columnName: "rdc_description", 
-         newDataType: "TEXT", 
-         confirm: "Successfully updated the rdc_description column." 
-      )
-  }
 }
