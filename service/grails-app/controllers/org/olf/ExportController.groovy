381c7deceff4f084d27b1dd9f523a52b526f8a7a
John Fereira <jaf30@cornell.edu>
2019-07-16 18:14:20 +0100

    call exportService.entitiled() instead of titles method

--- service/grails-app/controllers/org/olf/ExportController.groovy
+++ service/grails-app/controllers/org/olf/ExportController.groovy
@@ -1,12 +1,12 @@
 package org.olf
 
 import com.k_int.okapi.OkapiTenantAwareController
-
+ 
 import grails.gorm.multitenancy.CurrentTenant
 import groovy.util.logging.Slf4j 
 import grails.converters.JSON
 import org.olf.kb.TitleInstance
-
+import static org.springframework.http.HttpStatus.OK
 
 /**
  * The ExportController provides endpoints for exporting content in specific formats
@@ -14,24 +14,26 @@ import org.olf.kb.TitleInstance
  */
 @Slf4j
 @CurrentTenant
-class ExportController extends OkapiTenantAwareController<TitleInstance> {
-
+class ExportController extends OkapiTenantAwareController<TitleInstance>  {
+	
+	
+  ExportService exportService
+	
+  final String csvMimeType = 'text/csv'
+  final String tsvMimeType = 'text/tab-separated-value'
+  final String encoding = "UTF-8"
   
 
   ExportController()  {
 	  super(TitleInstance, true)
   }
-
-  public static final String TENANT = "X-Okapi-Tenant";
-  
-  ExportService exportService
 	
   /**
    * main index method (by default, return titles as json)
    */
   def index() {
       log.debug("ExportController::index");
-	  def results = exportService.titles()
+	  def results = exportService.entitled()
 	  respond results 
   }
 
@@ -39,10 +41,28 @@ class ExportController extends OkapiTenantAwareController<TitleInstance> {
    * kbart export (placeholder)
    */
   def kbart() {
-      log.debug("ExportController::kbart"); 
-	  def result = ['ExportController':'kbart']
-	  respond result
+
+	  final String filename = 'export.tsv' 
+	  String headline = exportService.kbartheader()
+	  withFormat {
+		  csv { 
+			  response.status = OK.value()
+			  response.contentType = "${tsvMimeType};charset=${encoding}";
+			  response.setHeader "Content-disposition", "attachment; filename=${filename}"
+			  def out = response.outputStream
+			  out.withWriter {
+					  writer -> writer.write(headline)
+					  writer.flush()
+					  writer.close()
+			  }
+			  out.flush()
+		  }
+	  }
+	  
+	   
   }
+  
+   
 
 }
 
