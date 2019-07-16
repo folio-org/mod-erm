49cb140a3f1c714d18b4987ab92e002bbaf558ff
John Fereira <jaf30@cornell.edu>
2019-07-16 18:17:57 +0100

    initial implementation of entitled method

--- service/grails-app/services/org/olf/ExportService.groovy
+++ service/grails-app/services/org/olf/ExportService.groovy
@@ -1,13 +1,23 @@
 package org.olf
+import org.olf.kb.ErmResource
+import org.olf.kb.PackageContentItem
+import org.olf.kb.Pkg
+import org.olf.kb.TitleInstance 
 
+import com.k_int.okapi.OkapiTenantAwareController
+
+import grails.gorm.DetachedCriteria
+import grails.gorm.multitenancy.CurrentTenant
+import groovy.util.logging.Slf4j
 import grails.gorm.transactions.Transactional 
 import org.hibernate.Hibernate
 import org.hibernate.sql.JoinType 
-import org.olf.kb.* 
-import org.olf.erm.* 
+ 
+
 // uncomment these when code for ERM-215 is ready
-// import org.olf.export.KBartExport
-// import org.olf.export.ExportConstants
+//import org.olf.export.KBart
+//import org.olf.export.KBartExport
+
 
 
 /**
@@ -15,15 +25,59 @@ import org.olf.erm.*
  */
 @Transactional
 public class ExportService { 
+	CoverageService coverageService
 	
-  private List<TitleInstance> titles() { 
-	  List<TitleInstance> titles = TitleInstance.entitled.list()
-	  return titles 
+   List<ErmResource> entitled() { 
+	  // Ian: It's now possible for an agreement to have entitlements that do not link to a resource. Need
+      // to talk through with steve about how this should work.
+      final def results = ErmResource.createCriteria ().list {
+        or {
+          
+          // Direct relations...
+          // Use a sub query to avoid multiple entries per item.
+          'in' 'id', new DetachedCriteria(ErmResource).build {
+            
+            createAlias 'entitlements', 'direct_ent'
+              ne 'class', Pkg
+              isNotNull 'direct_ent.owner.id'
+            
+            projections {
+              property ('id')
+            }
+          }
+          
+          
+          // Pci linked via package.
+          'in' 'id', new DetachedCriteria(PackageContentItem).build {
+        
+            'in' 'pkg.id', new DetachedCriteria(Pkg).build {
+              createAlias 'entitlements', 'pkg_ent'
+                isNotNull 'pkg_ent.owner.id'
+                
+                projections {
+                  property ('id')
+                }
+            }
+            
+            projections {
+              property ('id')
+            }
+          }
+        }
+        
+        readOnly (true)
+      }
+      
+      // This method writes to the web request if there is one (which of course there should be as we are in a controller method)
+      coverageService.lookupCoverageOverrides(results)
+      
+     
+	  return results 
   }
   
-  private String kbart() {
+  String kbartheader() {
 	  /*
-	   The exportshould include:
+	   The export should include:
 	  
 	  Agreement name (desirable, not required)
 	  Agreement ID (desirable, not required)
@@ -35,7 +89,46 @@ public class ExportService {
 	  E-resource type
 	  Platform
 	  Package
-	  */
+	  */ 
+
+      String headline =
+      "publication_title\t" +
+      "print_identifier\t" +
+      "online_identifier\t" +
+      "date_first_issue_online\t" +
+      "num_first_vol_online\t" +
+      "num_first_issue_online\t" +
+      "date_last_issue_online\t" +
+      "num_last_vol_online\t" +
+      "num_last_issue_online\t" +
+      "title_url\t" +
+      "first_author\t" +
+      "title_id\t" +
+      "embargo_info\t" +
+      "coverage_depth\t" +
+      "notes\t" +
+      "publisher_name\t" +
+      "publication_type\t" +
+      "date_monograph_published_print\t" +
+      "date_monograph_published_online\t" +
+      "monograph_volume\t" +
+      "monograph_edition\t" +
+      "first_editor\t" +
+      "parent_publication_title_id\t" +
+      "preceding_publication_title_id\t" +
+      "access_type\t";
+       
+	  
+	  return headline
+
+	  
+      /*def fieldsNames = new StringBuilder() 
+	   
+	  def fields = KBart.declaredFields
+	  fields.each {
+		 fieldsNames << it.name << "\t"
+	  } 
+	  return fieldsNames.toString()*/
 	  
   }
 
