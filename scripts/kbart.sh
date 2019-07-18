e67f1b137c58aa0aa643d9045424346498cb7f1e
John Fereira <jaf30@cornell.edu>
2019-07-18 12:19:27 +0100

    add verbose option

--- scripts/kbart.sh
+++ scripts/kbart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --header "X-Okapi-Tenant: diku" http://localhost:8080/erm/export/kbart -X GET
+curl --header "X-Okapi-Tenant: diku" http://localhost:8080/erm/export/kbart -X GET -v
