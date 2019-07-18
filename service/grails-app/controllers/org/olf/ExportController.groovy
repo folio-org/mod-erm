cae35483107024f48ad4ea6ca466918617cccd5e
John Fereira <jaf30@cornell.edu>
2019-07-18 12:25:27 +0100

    add code to test for agreement id.  Add initial mapToKbart method

--- service/grails-app/controllers/org/olf/ExportController.groovy
+++ service/grails-app/controllers/org/olf/ExportController.groovy
@@ -6,6 +6,14 @@ import grails.gorm.multitenancy.CurrentTenant
 import groovy.util.logging.Slf4j 
 import grails.converters.JSON
 import org.olf.kb.TitleInstance
+import org.olf.kb.PlatformTitleInstance
+import org.olf.kb.IdentifierOccurrence
+import org.olf.kb.Identifier
+import org.olf.kb.IdentifierNamespace
+import org.olf.kb.CoverageStatement
+import org.olf.kb.Platform
+import org.olf.kb.ErmResource
+import org.olf.export.KBart
 import static org.springframework.http.HttpStatus.OK
 
 /**
@@ -33,7 +41,27 @@ class ExportController extends OkapiTenantAwareController<TitleInstance>  {
    */
   def index() {
       log.debug("ExportController::index");
-	  def results = exportService.entitled()
+	  
+	  final String subscriptionAgreementId = params.get("subscriptionAgreementId")
+	  println "id: "+subscriptionAgreementId
+	  
+	  List<ErmResource> results
+	  if (subscriptionAgreementId) {
+		  log.debug("Getting export for specific agreement: "+ subscriptionAgreementId)
+		  results = exportService.entitled()
+		  log.debug("found this many resources: "+ results.size())
+		  //mapToKBart(results)
+		  
+	  } else {
+		  log.debug("Getting export for all agreements")
+	      results = exportService.entitled()
+		  log.debug("found this many resources: "+ results.size())
+		  
+		 // mapToKBart(results)
+
+	  }
+	  
+	   
 	  respond results 
   }
 
@@ -45,7 +73,7 @@ class ExportController extends OkapiTenantAwareController<TitleInstance>  {
 	  final String filename = 'export.tsv' 
 	  String headline = exportService.kbartheader()
 	  withFormat {
-		  csv { 
+		  tsv { 
 			  response.status = OK.value()
 			  response.contentType = "${tsvMimeType};charset=${encoding}";
 			  response.setHeader "Content-disposition", "attachment; filename=${filename}"
@@ -60,6 +88,54 @@ class ExportController extends OkapiTenantAwareController<TitleInstance>  {
 	  }
 	  
 	   
+  }
+  
+  public void mapToKBart(final List<ErmResource> resources) {
+	  for (ErmResource res: resources) {
+		  
+		PlatformTitleInstance pti = res.pti
+		TitleInstance ti = pti.titleInstance
+		
+		KBart kbart = new KBart()
+	    //println res.getClass().getName()
+		//kbart.publication_title = res.name
+		if (res.depth) kbart.coverage_depth = res.depth
+		if (res.note) kbart.notes = res.note
+		if (pti) {
+		  //println "found pti"
+		  if (pti.url) kbart.title_url = pti.url
+		}
+		
+		if (ti) {
+			//println "found titleInstance"
+			kbart.publication_title = ti.name
+			if (ti.type.value) kbart.publication_type = ti.type.value
+			Object obj = ti.identifiers
+			if (obj) {
+				//println "got ti.identifiers: "+ obj.getClass().getName()
+				Iterator iter = ti.identifiers.iterator();
+				while (iter.hasNext()) {
+					IdentifierOccurrence thisIdent = iter.next()
+					Identifier ident =  thisIdent.identifier
+					if (ident) {
+						if (ident.ns.value.equals("eissn")) {
+							kbart.online_identifier = ident.value
+						} else if (ident.ns.value.equals("isbn")) {
+							kbart.print_identifier = ident.value
+						} else if (ident.ns.value.equals("issn")) {
+							kbart.print_identifier = ident.value
+						}
+						 
+							
+					}
+				}
+			}
+			println "\n"
+		}
+		println kbart.toString() 
+		
+		//println kbart as JSON
+	  }
   }
   
    
