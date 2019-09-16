4dd4c09129739e23ed9442d7052da4a48de3a3c5
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-09-16 19:11:39 +0100

    Handle null or empty amendments.

--- service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
+++ service/grails-app/domain/org/olf/erm/RemoteLicenseLink.groovy
@@ -35,11 +35,11 @@ public class RemoteLicenseLink extends RemoteOkapiLink implements MultiTenant<Re
   }
   
   private String getApplicableAmendmentParams() {
-    amendments.findResults({it.status.value == 'current' ? 'applyAmendment=' + it.amendmentId : '' }).join('&')
+    amendments?.findResults({it.status.value == 'current' ? 'applyAmendment=' + it.amendmentId : '' })?.join('&')
   }
 
   @Override
   public final def remoteUri() {
-    {->"licenses/licenses/${remoteId}?${applicableAmendmentParams}"}
+    {->"licenses/licenses/${remoteId}${applicableAmendmentParams ? '?' + applicableAmendmentParams : ''}"}
   }
 }
