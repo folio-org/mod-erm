376eaa0f9dad18fce32e210507f7cd6899ceff9a
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-01 12:08:04 +0100

    added script to submit some test data to the database

---
+++ scripts/submit_cancelled_agreements.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+AUTH_TOKEN=`./okapi-login`
+
+OKAPI="http://localhost:9130"
+TENANT="diku"
+
+if [ -f .okapirc ]; then
+  . .okapirc
+elif [ -f $HOME/.okapirc ]; then
+  . $HOME/.okapirc
+fi
+
+
+
+#  serviceType:"Loan",
+AGREE_1=`curl --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST ${OKAPI}/erm/sas -d ' {
+  name: "Agreement Test 1",
+  agreementStatus: "cancelled",
+  notARealProperty:"Test that this is ignored in accordance with Postels law",
+  systemInstanceIdentifier:"893475987348973",
+}
+'`
+AGREE_2=`curl --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST ${OKAPI}/erm/sas -d ' {
+  name: "Agreement Test 2",
+  agreementStatus: "closed",
+  systemInstanceIdentifier:"893475987348974",
+}
+'`
+AGREE_3=`curl --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST ${OKAPI}/erm/sas -d ' {
+  name: "Agreement Test 3",
+  agreementStatus: "active",
+  systemInstanceIdentifier:"893475987348975",
+}
+'`
+
+echo Result : $AGREE_1
+echo Result : $AGREE_2
+echo Result : $AGREE_3
+
+echo Parse result to extract request ID
+AGREE_1_ID=`echo $AGREE_1 | jq -r ".id" | tr -d '\r'`
+AGREE_2_ID=`echo $AGREE_2 | jq -r ".id" | tr -d '\r'`
+AGREE_3_ID=`echo $AGREE_3 | jq -r ".id" | tr -d '\r'`
+echo Created request 1: $AGREE_1_ID
+echo Created request 2: $AGREE_2_ID
+echo Created request 3: $AGREE_3_ID
