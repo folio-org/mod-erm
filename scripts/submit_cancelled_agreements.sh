6487d1898e6acc3656d3c29bce0fabdd63ba8ea4
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-02 08:34:41 +0100

    Updated script to contain more than one cancelled agreement

--- scripts/submit_cancelled_agreements.sh
+++ scripts/submit_cancelled_agreements.sh
@@ -33,15 +33,24 @@ AGREE_3=`curl --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOK
   systemInstanceIdentifier:"893475987348975",
 }
 '`
+AGREE_4=`curl --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST ${OKAPI}/erm/sas -d ' {
+  name: "Agreement Test 4",
+  agreementStatus: "cancelled",
+  systemInstanceIdentifier:"893475987348973",
+}
+'`
 
 echo Result : $AGREE_1
 echo Result : $AGREE_2
 echo Result : $AGREE_3
+echo Result : $AGREE_4
 
 echo Parse result to extract request ID
 AGREE_1_ID=`echo $AGREE_1 | jq -r ".id" | tr -d '\r'`
 AGREE_2_ID=`echo $AGREE_2 | jq -r ".id" | tr -d '\r'`
 AGREE_3_ID=`echo $AGREE_3 | jq -r ".id" | tr -d '\r'`
+AGREE_4_ID=`echo $AGREE_3 | jq -r ".id" | tr -d '\r'`
 echo Created request 1: $AGREE_1_ID
 echo Created request 2: $AGREE_2_ID
 echo Created request 3: $AGREE_3_ID
+echo Created request 3: $AGREE_4_ID
