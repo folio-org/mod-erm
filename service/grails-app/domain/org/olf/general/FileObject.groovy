72deb3e5b12a0b1f43457d97f7b3a666f36c0d73
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-11-26 13:19:57 +0000

    Tidy.

--- service/grails-app/domain/org/olf/general/FileObject.groovy
+++ service/grails-app/domain/org/olf/general/FileObject.groovy
@@ -1,10 +1,12 @@
 package org.olf.general
-import grails.gorm.MultiTenant
-import grails.gorm.multitenancy.Tenants
 import java.sql.Blob
+
 import javax.persistence.Lob
+
 import org.hibernate.engine.jdbc.BlobProxy
 import org.springframework.web.multipart.MultipartFile
+
+import grails.gorm.MultiTenant
 
 class FileObject implements MultiTenant<FileObject> {
 
@@ -20,11 +22,11 @@ class FileObject implements MultiTenant<FileObject> {
     this.fileContents = fileContents
   }
   
-  void setFileContents(InputStream is, long length) {
+  void setFileContents( InputStream is, long length ) {
     setFileContents( BlobProxy.generateProxy(is, length) )
   }
   
-  void setFileContents(MultipartFile file) {
+  void setFileContents( MultipartFile file ) {
     setFileContents( file.inputStream, file.size )
   }
 
