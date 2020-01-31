b7bf94bde20fff3fec809d5f5e27e4954e883dd5
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-01-31 10:51:36 +0000

    imported tsv file now converted to array of arrays. We also now have an object that contains all of the mapping information we'll need

--- service/grails-app/controllers/org/olf/PackageController.groovy
+++ service/grails-app/controllers/org/olf/PackageController.groovy
@@ -40,25 +40,68 @@ class PackageController extends OkapiTenantAwareController<Pkg> {
     Reader fr = new InputStreamReader(bis);
     CSVReader csvReader = new CSVReaderBuilder(fr).build();
 
+    def importedFileArray = []
+    // We take the file and turn it into an array of arrays
 
-    // peek gets without removing from iterator
-    // readNext etc remove from csvreader
-    String[] header = (String[])(csvReader.peek())
-
-    log.debug("file header: ${header}")
-    log.debug("file header class : ${header.getClass()}")
-
-    log.debug("file header with index: ${csvReader[0]}")
-    log.debug("file header with index class: ${csvReader[0].getClass()}")
-    //log.debug("file header?: ${header.indexOf('publication_title')}")
-
-
+    // peek gets line without removing from iterator
+    // readNext gets line and removes it from the csvReader object
     String[] record;
     while ((record = csvReader.readNext()) != null) {
         for (String value : record) {
-            log.debug(value);
+          importedFileArray << value.split("\t")
         }
     }
+
+    log.debug("file as an array: ${importedFileArray}")
+    def header = importedFileArray[0]
+    
+    // Create an object containing fields we can accept and their mappings in our domain structure, as well as indices in the imported file, with -1 if not found
+    Map acceptedFields = [
+      publication_title: ['title', -1],
+      print_identifier: ['siblingInstanceIdentifiers', -1],
+      online_identifier: ['instanceIdentifiers', -1],
+      date_first_issue_online: ['CoverageStatement.startDate', -1],
+      num_first_vol_online: ['CoverageStatement.startVolume', -1],
+      num_first_issue_online: ['CoverageStatement.startIssue', -1],
+      date_last_issue_online: ['CoverageStatement.endDate', -1],
+      num_last_vol_online: ['CoverageStatement.endVolume', -1],
+      num_last_issue_online: ['CoverageStatement.endIssue', -1],
+      title_url: ['url', -1],
+      first_author: ['firstAuthor', -1],
+      title_id: [null, -1],
+      embargo_info: ['embargo', -1],
+      coverage_depth: ['coverageDepth', -1],
+      notes: ['coverageNote', -1],
+      publisher_name: [null, -1],
+      publication_type: ['TitleInstance.type', -1],
+      date_monograph_published_print: ['dateMonographPublished', -1],
+      date_monograph_published_online: ['dateMonographPublished', -1],
+      monograph_volume: ['monographVolume', -1],
+      monograph_edition: ['monographVolume', -1],
+      first_editor: ['firstEditor', -1],
+      parent_publication_title_id: [null, -1],
+      preceding_publication_title_id: [null, -1],
+      access_type : [null, -1]
+    ]
+
+    log.debug("Accepted Fields: ${acceptedFields}")
+    log.debug("Accepted Fields: ${acceptedFields.getClass()}")
+    log.debug("Accepted Fields: ${acceptedFields.publication_title}")
+
+/*     def fieldIndicesForFile = [:]
+    acceptedFields.each{key, value ->
+    // Create mapping of field
+      int index = -1;
+      for (int i=0; i<header.length; i++) {
+        if (TYPES[i].equals(carName)) {
+            index = i;
+            break;
+        }
+      }
+    } */
+
+    
+
     render [:] as JSON;
   }
 
