b23e868a46ecb84619eef23423af017e2addec4f
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-02-11 17:17:18 +0000

    added migrations, and KbartImportJob now has extra fields to hold package information

--- service/grails-app/domain/org/olf/general/jobs/KbartImportJob.groovy
+++ service/grails-app/domain/org/olf/general/jobs/KbartImportJob.groovy
@@ -10,23 +10,10 @@ import com.opencsv.CSVReaderBuilder
 
 class KbartImportJob extends PersistentJob implements MultiTenant<KbartImportJob> {
   
-  /* private final MultipartFile file = request.getFile('upload')
-
-    Map packageInfo = [
-      packageName: request.getParameter("packageName"),
-      packageSource: request.getParameter("packageSource"),
-      packageReference: request.getParameter("packageReference")
-    ]
-    
-    BOMInputStream bis = new BOMInputStream(file.getInputStream());
-    Reader fr = new InputStreamReader(bis);
-    CSVReader csvReader = new CSVReaderBuilder(fr).build();
-    
-
-    def completed = importService.importPackageFromKbart(csvReader, packageInfo)
-   */
-
-
+  String packageName
+  String packageSource
+  String packageReference
+  
   final Closure getWork() {
     
     final Closure theWork = { final String eventId, final String tenantId ->
@@ -36,15 +23,24 @@ class KbartImportJob extends PersistentJob implements MultiTenant<KbartImportJob
       
         // We should ensure the job is read into the current session. This closure will probably execute
         // in a future session and we need to reread the event in.
-        final PersistentJob job = PersistentJob.read(eventId)
-        log.debug("What even is a job?: ${job}")
-        if (job.fileUpload) {
+        final KbartImportJob job = KbartImportJob.read(eventId)
 
-          BOMInputStream bis = new BOMInputStream(job.fileUpload.fileObject.getInputStream())
+        Map packageInfo = [
+          packageName: job.packageName,
+          packageSource: job.packageSource,
+          packageReference: job.packageReference
+        ]
+
+        //TODO Actual validation here
+        log.debug("Check that the extra fields Map is correct: ${packageInfo}")
+        
+
+        if (job.fileUpload) {
+           log.debug("Incoming Job Stream: ${job.fileUpload.fileObject.fileContents.binaryStream}")
+          BOMInputStream bis = new BOMInputStream(job.fileUpload.fileObject.fileContents.binaryStream);
           Reader fr = new InputStreamReader(bis);
           CSVReader csvReader = new CSVReaderBuilder(fr).build();
-
-          //importService.importFromFile(js.parse( job.fileUpload.fileObject.fileContents.binaryStream ))
+          importService.importPackageFromKbart(csvReader, packageInfo)
         } else {
           log.error "No file attached to the Job."
         }
@@ -63,5 +59,12 @@ class KbartImportJob extends PersistentJob implements MultiTenant<KbartImportJob
   
   static constraints = {
       fileUpload (nullable:false)
+  }
+
+  static mapping = {
+                      version false
+                 packageName column:'package_name'
+               packageSource column:'package_source'
+            packageReference column:'package_reference'
   }
 }
