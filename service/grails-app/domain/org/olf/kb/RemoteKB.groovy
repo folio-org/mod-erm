3d0cea3e23baa8412363125e0b30070da75dc6c4
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-04-08 17:29:50 +0100

    replaced beforeUpdate method with individual methods for each property detailing which can and which can't be updated while in a state of 'readonly'

--- service/grails-app/domain/org/olf/kb/RemoteKB.groovy
+++ service/grails-app/domain/org/olf/kb/RemoteKB.groovy
@@ -93,11 +93,137 @@ public class RemoteKB implements MultiTenant<RemoteKB> {
     return "RemoteKB ${name} - ${type}/${uri}/${cursor}".toString()
   }
 
-  def beforeUpdate() {
+  // We want SOME properties to be editable, some not to be, so we have custom setters to declare which we can and can't set
+  def setName( String name ) {
     if (this.readonly == true) {
-      log.debug("Denying to update KB ${this.id} / ${this.name} because 'readonly' is set to 'true'")
-      return false
+      log.debug("Denying update of name as 'readonly' is set to 'true'")
+      return
     }
+    this.name = name
+  }
+
+  def setType( String type ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of type as 'readonly' is set to 'true'")
+      return
+    }
+    this.type = type
+  }
+
+  def setCursor( String cursor ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of cursor as 'readonly' is set to 'true'")
+      return
+    }
+    this.cursor = cursor
+  }
+
+  def setUri( String uri ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of uri as 'readonly' is set to 'true'")
+      return
+    }
+    this.uri = uri
+  }
+
+  def setListPrefix( String listPrefix ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of listPrefix as 'readonly' is set to 'true'")
+      return
+    }
+    this.listPrefix = listPrefix
+  }
+
+  def setFullPrefix( String fullPrefix ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of fullPrefix as 'readonly' is set to 'true'")
+      return
+    }
+    this.fullPrefix = fullPrefix
+  }
+
+  def setPrincipal( String principal ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of principal as 'readonly' is set to 'true'")
+      return
+    }
+    this.principal = principal
+  }
+
+  def setCredentials( String credentials ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of credentials as 'readonly' is set to 'true'")
+      return
+    }
+    this.credentials = credentials
+  }
+
+  def setSyncStatus( String syncStatus ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of syncStatus as 'readonly' is set to 'true'")
+      return
+    }
+    this.syncStatus = syncStatus
+  }
+
+  def setLastCheck( Long lastCheck ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of lastCheck as 'readonly' is set to 'true'")
+      return
+    }
+    this.lastCheck = lastCheck
+  }
+
+  def setRectype( Long rectype ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of rectype as 'readonly' is set to 'true'")
+      return
+    }
+    this.rectype = rectype
+  }
+
+  def setReadonly( boolean readonly ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of readonly as 'readonly' is set to 'true'")
+      return
+    }
+    this.readonly = readonly
+  }
+
+  def setTrustedSourceTI( boolean trustedSourceTI ) {
+    this.trustedSourceTI = trustedSourceTI
+  }
+
+  def setSupportsHarvesting( Boolean supportsHarvesting ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of supportsHarvesting as 'readonly' is set to 'true'")
+      return
+    }
+    this.supportsHarvesting = supportsHarvesting
+  }
+
+  def setActive( Boolean active ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of active as 'readonly' is set to 'true'")
+      return
+    }
+    this.active = active
+  }
+
+  def setActivationSupported( Boolean activationSupported ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of activationSupported as 'readonly' is set to 'true'")
+      return
+    }
+    this.activationSupported = activationSupported
+  }
+
+  def setActivationEnabled( Boolean activationEnabled ) {
+    if (this.readonly == true) {
+      log.debug("Denying update of activationEnabled as 'readonly' is set to 'true'")
+      return
+    }
+    this.activationEnabled = activationEnabled
   }
 
   def beforeDelete() {
