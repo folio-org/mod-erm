4b79fa725e14098ef6cc1394a907fb3397bd21fb
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2020-06-05 11:23:22 +0200

    ERM-775 Use information from siblingInstances in KBART export
    * find pissn identifier

--- service/src/main/groovy/org/olf/export/KBart.groovy
+++ service/src/main/groovy/org/olf/export/KBart.groovy
@@ -194,21 +194,6 @@ public class KBart implements Serializable {
     return header as String[]
   }
 
-  static String findPissnIdentifier(Object identifiers) {
-    if (identifiers) {
-      Iterator iter = identifiers.iterator();
-      String identifierValue;
-      while (iter.hasNext()) {
-        IdentifierOccurrence thisIdent = iter.next()
-        Identifier ident =  thisIdent.identifier
-          if (ident?.ns?.value == "pissn") {
-            return ident.value
-          }
-      }
-    }  
-    return " ";
-  }
-
   static String getIdentifierValue(Object identifiers) {
     if (identifiers) {
       Iterator iter = identifiers.iterator();
@@ -289,36 +274,18 @@ public class KBart implements Serializable {
 
         Object identifiers_obj = ti.identifiers
 
-        /* List<String> siblingPrintIdentifiers = new ArrayList<String>();
-        List<String> siblingElectronicIdentifiers = new ArrayList<String>();
-
-        ti.relatedTitles.each { relatedTitle ->
-            if (relatedTitle.subType.value == "print" && !siblingPrintIdentifier) {
-              siblingPrintIdentifiers.add(getIdentifierValue(relatedTitle.identifiers));
-            } else if (relatedTitle.subType.value == "electronic" && !siblingElectronicIdentifier) {
-              siblingElectronicIdentifiers.add(getIdentifierValue(relatedTitle.identifiers));
-            }
-        }
-
-        // debug
-        siblingPrintIdentifiers.add('debug-value');
-        siblingPrintIdentifiers.each { printIdentifier ->
-          log.debug(printIdentifier)
-        }
-        log.debug('ganzes Array: ' + siblingPrintIdentifiers); */
-
         if (ti.subType?.value == "print") {
           kbart.print_identifier = getIdentifierValue(identifiers_obj);
-          ti.relatedTitles.each { relatedTitle ->
+          ti.relatedTitles?.each { relatedTitle ->
             if (relatedTitle.subType.value == "electronic" && !kbart.online_identifier) {
               kbart.online_identifier = getIdentifierValue(relatedTitle.identifiers);
             }
           }
         } else if (ti.subType?.value == "electronic") {
           kbart.online_identifier = getIdentifierValue(identifiers_obj);
-          kbart.print_identifier = findPissnIdentifier(identifiers_obj);
+          kbart.print_identifier = identifiers_obj.find { it.identifier.ns.value == "pissn"}?.identifier.value;
           if (!kbart.print_identifier) {
-            ti.relatedTitles.each { relatedTitle ->
+            ti.relatedTitles?.each { relatedTitle ->
               if (relatedTitle.subType.value == "print" && !kbart.print_identifier) {
                 kbart.print_identifier = getIdentifierValue(relatedTitle.identifiers);
               }
