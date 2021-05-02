993848765bd6050953596b5185a9666d9f90dc0d
Ian Ibbotson <ianibbo@gmail.com>
2021-05-02 14:49:08 +0100

    extend vagrant params

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -17,11 +17,24 @@ INS_ID=`echo $DEP_DESC | jq -rc '.instId'`
 
 echo Service id $SVC_ID Inst id $INS_ID
 
+printf "\n\nremove tenant deployment\n"
 curl -XDELETE "http://localhost:9130/_/proxy/tenants/diku/modules/${SVC_ID}"
+
+printf "\n\ndelete service instance\n"
 curl -XDELETE "http://localhost:9130/_/discovery/modules/${SVC_ID}/${INS_ID}"
+
+printf "\n\ndelete module descriptor\n"
 curl -XDELETE "http://localhost:9130/_/proxy/modules/${SVC_ID}"
+
 # ./gradlew clean generateDescriptors
+
+printf "\n\nPost module descriptor\n"
 curl -XPOST 'http://localhost:9130/_/proxy/modules' -d @"${DESCRIPTORDIR}/ModuleDescriptor.json"
+
+printf "\n\nPost deployment descriptor\n"
 curl -XPOST 'http://localhost:9130/_/discovery/modules' -d "$DEP_DESC"
+
+printf "\n\nEnable\n"
 curl -XPOST 'http://localhost:9130/_/proxy/tenants/diku/install?tenantParameters=loadSample%3Dtest,loadReference%3Dother' -d `echo $DEP_DESC | jq -c '[{id: .srvcId, action: "enable"}]'`
+
 popd
