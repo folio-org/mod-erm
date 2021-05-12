14f787429ad658ab5a7c96615aa4689c8e9c254a
Peter Boehm <boehm@hebis.uni-frankfurt.de>
2021-05-12 15:39:51 +0200

    ERM-1612: alternative solution

--- service/grails-app/services/org/olf/RemoteKbCleanupService.groovy
+++ service/grails-app/services/org/olf/RemoteKbCleanupService.groovy
@@ -6,10 +6,19 @@ import org.olf.kb.RemoteKB
  * is not set to TRUE. It sets readonly to TRUE for this RemoteKB.
  */
 public class RemoteKbCleanupService {
+    private static final String LOCAL_KB_UPDATE = '''UPDATE remotekb
+SET rkb_readonly=TRUE
+WHERE rkb_name LIKE 'LOCAL';
+    '''
     
     def checkLocal() {
         log.debug("RemoteKbCleanupService: Check for RemoteKBs with name LOCAL")
-        RemoteKB kb = RemoteKB.findByName('LOCAL')
+        
+        // #1 - Raw SQL
+        RemoteKB.executeQuery(PENDING_JOBS_HQL)
+        
+        // #2 - ORM
+        RemoteKB kb = RemoteKB.findByName('LOCAL_KB_UPDATE')
         if (kb) {
             if (!kb.readonly) {
                 kb.readonly = Boolean.TRUE
