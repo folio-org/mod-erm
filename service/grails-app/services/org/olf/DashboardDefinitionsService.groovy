868a4895f7961e9cb242adf39afc0de177f8457b
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-05-12 08:54:32 +0100

    fixes to the DashboardDefinitionsService

--- service/grails-app/services/org/olf/DashboardDefinitionsService.groovy
+++ service/grails-app/services/org/olf/DashboardDefinitionsService.groovy
@@ -3,19 +3,14 @@ package org.olf
 import org.grails.io.support.PathMatchingResourcePatternResolver
 import org.grails.io.support.Resource
 import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
-import static groovy.io.FileType.FILES
 
-import groovy.transform.CompileStatic
-
-@CompileStatic
 class DashboardDefinitionsService {
   PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver()
   def jsonSlurper = new JsonSlurper()
 
   public getWidgetDefinitions() {
     Resource[] widgetDefs = resolver.getResources("classpath:sample_data/widgetDefinitions/*")
-    ArrayList<JSONObject> definitions = [];
+    ArrayList definitions = [];
     widgetDefs.each { resource ->
       def stream = resource.getInputStream()
       def wd = jsonSlurper.parse(stream)
